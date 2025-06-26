a5212a1e6b7833fd0c24055afd9768be7deb6a12
Sadot18 <19161291@itoaxaca.edu.mx>
2025-06-25 21:43:52 -0600

    Agregar componente de inicio de sesión con formulario y estilos

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatInputModule, 
+    MatButtonModule,
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  }); 
+
+  onSubmit(){
+    if(this.loginForm.valid){
+      console.log(this.loginForm.value); //Aqui llamamos al servicio de autenticación
+    }
+  }
+}
