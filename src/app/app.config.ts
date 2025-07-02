5c06f081708e14a1003b696f638a3ad7457df2d4
Sadot18 <19161291@itoaxaca.edu.mx>
2025-07-02 16:23:28 -0600

    Agregar configuración de OpenAPI y generar recursos para la API de gestión de citas

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
+import { ApiModule } from '../openapi/generated-resources/api.module';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes), 
+    importProvidersFrom(ApiModule.forRoot({ rootUrl: 'http://localhost:8080' })),
+    provideRouter(routes),
     provideClientHydration(),
     provideAnimations(),
     provideHttpClient(),
